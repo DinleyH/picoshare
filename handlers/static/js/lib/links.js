ded8b81329053960d3b2885fee4382a76952360d
DinleyH <abinerimike@gmail.com>
2025-07-13 18:27:57 +0100

    Added playlist shortlink generation and copy button functionality

--- handlers/static/js/lib/links.js
+++ handlers/static/js/lib/links.js
@@ -2,6 +2,10 @@ export function makeShortLink(fileId) {
   return `${window.location.origin}/-${fileId}`;
 }
 
+export function makePlaylistShortLink(playlistId) {
+  return `${window.location.origin}/pl-${playlistId}`;
+}
+
 export function makeVerboseLink(fileId, filename) {
   return makeShortLink(fileId) + "/" + encodeURIComponent(filename);
 }
